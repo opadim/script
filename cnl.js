4f52857d42336d8cdec2256632612dcec2abc2d3
opadim <152433032+opadim@users.noreply.github.com>
2023-12-05 14:03:46 +0000

    Update cnl.js

--- cnl.js
+++ cnl.js
@@ -1,45 +1,46 @@
-(function () {
-    const a1b2 = new URLSearchParams(window.location.search);
+// cannoli = awesomex10
+document.addEventListener('DOMContentLoaded', function () {
+    (function () {
+        var a1b2c3 = new URLSearchParams(window.location.search);
 
-    function c3d4(e5f6) {
-        return e5f6.replace(/ /g, '_s_').replace(/-/g, '_d_');
-    }
-
-    if (a1b2.has('tid')) {
-        var g7h8 = a1b2.get('tid');
-        var i9j1 = c3d4(g7h8);
-        a1b2.set('tid', i9j1);
-    }
-    
-    var k2l3 = a1b2.get('gclid') || a1b2.get('msclkid') || a1b2.get('fbclid');
-
-    if (a1b2.toString()) {
-        var m4n5 = document.getElementsByTagName('a');
-        for (var o6p7 = 0; o6p7 < m4n5.length; o6p7++) {
-            var q8r9 = m4n5[o6p7];
-            var s0t1 = q8r9.hash;
-            var u2v3 = q8r9.href.split('#')[0];
-            var w4x5 = new URL(u2v3).searchParams;
-
-            var y6z7 = k2l3;
-
-            if (w4x5.has('tid') && k2l3) {
-                y6z7 = c3d4(k2l3);
-                console.log(`Replacement value is: ${y6z7}`);
-            }
-
-            if (y6z7 && u2v3.includes('[cnlid]')) {
-                u2v3 = u2v3.replace('[cnlid]', y6z7);
-                console.log(`urlWithoutHash is: ${u2v3}`);
-            }
-
-            var a8b9 = a1b2.toString();
-            if (u2v3.indexOf('?') === -1) {
-                u2v3 += '?' + a8b9;
-            } else {
-                u2v3 += '&' + a8b9;
-            }
-            q8r9.href = u2v3 + s0t1;
+        function d4e5f6(g7h8i9) {
+            return g7h8i9.replace(/ /g, '_s_').replace(/-/g, '_d_');
         }
-    }
-})();
+
+        if (a1b2c3.has('tid')) {
+            var j0k1l2 = a1b2c3.get('tid');
+            var m3n4o5 = d4e5f6(j0k1l2);
+            a1b2c3.set('tid', m3n4o5);
+        }
+
+        var p6q7r8 = a1b2c3.get('gclid') || a1b2c3.get('msclkid') || a1b2c3.get('fbclid');
+
+        if (a1b2c3.toString()) {
+            var s9t0u1 = document.getElementsByTagName('a');
+            for (var v2w3x4 = 0; v2w3x4 < s9t0u1.length; v2w3x4++) {
+                var y5z6a7 = s9t0u1[v2w3x4];
+                var b8c9d0 = y5z6a7.hash;
+                var e1f2g3 = y5z6a7.href.split('#')[0];
+                var h4i5j6 = new URL(e1f2g3).searchParams;
+
+                var k7l8m9 = p6q7r8;
+
+                if (h4i5j6.has('tid') && p6q7r8) {
+                    k7l8m9 = d4e5f6(p6q7r8);
+                }
+
+                if (k7l8m9) {
+                    e1f2g3 = e1f2g3.replace('[cnlid]', k7l8m9).replace('%5Bcnlid%5D', k7l8m9);
+                }
+
+                var n0o1p2 = a1b2c3.toString();
+                if (e1f2g3.indexOf('?') === -1) {
+                    e1f2g3 += '?' + n0o1p2;
+                } else {
+                    e1f2g3 += '&' + n0o1p2;
+                }
+                y5z6a7.href = e1f2g3 + b8c9d0;
+            }
+        }
+    })();
+});
