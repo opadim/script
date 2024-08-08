6aabeafed8849989bd01559aad072bd777aa9838
opadim <152433032+opadim@users.noreply.github.com>
2024-08-08 16:49:23 +0100

    Update cnl_new.js

--- cnl_new.js
+++ cnl_new.js
@@ -1,54 +1,52 @@
+// cannoli = awesomex10
 document.addEventListener('DOMContentLoaded', function () {
     (function () {
-        var currentPageBaseURL = window.location.origin + window.location.pathname; // Get the current page base URL without parameters
-        var urlParams = new URLSearchParams(window.location.search);
+        var a1b2c3 = new URLSearchParams(window.location.search);
 
-        function encodeValue(value) {
-            return value.replace(/ /g, '_s_').replace(/-/g, '_d_');
+        function d4e5f6(g7h8i9) {
+            return g7h8i9.replace(/ /g, '_s_').replace(/-/g, '_d_').replace(/\//g, '');
         }
 
-        if (urlParams.has('tid')) {
-            var tidValue = urlParams.get('tid');
-            var encodedTidValue = encodeValue(tidValue);
-            urlParams.set('tid', encodedTidValue);
-        }
-
-        var cnlidValue = "NoClickID"; // Default to "NoClickID"
-        if (urlParams.has('gclid') && urlParams.get('gclid') ||
-            urlParams.has('wbraid') && urlParams.get('wbraid') ||
-            urlParams.has('msclkid') && urlParams.get('msclkid') ||
-            urlParams.has('fbclid') && urlParams.get('fbclid')) {
-            cnlidValue = urlParams.get('gclid') || urlParams.get('wbraid') || urlParams.get('msclkid') || urlParams.get('fbclid');
-        } else if (urlParams.has('gclid') || urlParams.has('wbraid') || urlParams.has('msclkid') || urlParams.has('fbclid')) {
-            cnlidValue = "EmptyClickID";
-        }
-
-        var links = document.getElementsByTagName('a');
-        for (var i = 0; i < links.length; i++) {
-            var link = links[i];
-            var hash = link.hash;
-            var urlWithoutHash = link.href.split('#')[0];
-            var linkParams = new URL(urlWithoutHash).searchParams;
-
-            var replacementValue = cnlidValue;
-
-            if (linkParams.has('tid') && cnlidValue) {
-                replacementValue = encodeValue(cnlidValue);
-            }
-
-            // Always perform the replacement
-            urlWithoutHash = urlWithoutHash.replace('[cnlid]', replacementValue).replace('%5Bcnlid%5D', replacementValue);
-
-            // Only append paramString if the link is not an anchor link or it navigates away from the current page
-            var paramString = urlParams.toString();
-            if (urlWithoutHash.indexOf(currentPageBaseURL) === -1 || !hash) { // Check if the link is not just an anchor or if it navigates away
-                if (urlWithoutHash.indexOf('?') === -1 && paramString) {
-                    urlWithoutHash += '?' + paramString;
-                } else if (paramString) {
-                    urlWithoutHash += '&' + paramString;
+        function replaceCnlid(url) {
+            var urlObj = new URL(url, document.location.href);
+            var params = new URLSearchParams(urlObj.search);
+            
+            if (params.has('tid')) {
+                var tidValue = params.get('tid');
+                if (tidValue.includes('[cnlid]') || tidValue.includes('%5Bcnlid%5D')) {
+                    params.set('tid', tidValue.replace(/(\[cnlid\]|%5Bcnlid%5D)/g, function(match) {
+                        return d4e5f6(decodeURIComponent(match));
+                    }));
                 }
             }
-            link.href = urlWithoutHash + hash;
+
+            urlObj.search = params.toString();
+            return urlObj.toString();
+        }
+
+        var p6q7r8 = a1b2c3.get('gclid') || a1b2c3.get('gbraid') || a1b2c3.get('wbraid') || a1b2c3.get('msclkid') || a1b2c3.get('fbclid');
+
+        if (a1b2c3.toString()) {
+            var s9t0u1 = document.getElementsByTagName('a');
+            for (var v2w3x4 = 0; v2w3x4 < s9t0u1.length; v2w3x4++) {
+                var y5z6a7 = s9t0u1[v2w3x4];
+                var b8c9d0 = y5z6a7.hash;
+                var e1f2g3 = y5z6a7.href.split('#')[0];
+
+                e1f2g3 = replaceCnlid(e1f2g3);
+
+                if (p6q7r8) {
+                    e1f2g3 = e1f2g3.replace(/(\[cnlid\]|%5Bcnlid%5D)/g, encodeURIComponent(p6q7r8));
+                }
+
+                var n0o1p2 = a1b2c3.toString();
+                if (e1f2g3.indexOf('?') === -1) {
+                    e1f2g3 += '?' + n0o1p2;
+                } else {
+                    e1f2g3 += '&' + n0o1p2;
+                }
+                y5z6a7.href = e1f2g3 + b8c9d0;
+            }
         }
     })();
 });
