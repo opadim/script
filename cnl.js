07f609377222208c081416a9e62300f80cff3142
opadim <152433032+opadim@users.noreply.github.com>
2024-08-09 21:24:06 +0100

    Rollback

--- cnl.js
+++ cnl.js
@@ -7,17 +7,16 @@ document.addEventListener('DOMContentLoaded', function () {
             return g7h8i9.replace(/ /g, '_s_').replace(/-/g, '_d_').replace(/\//g, '');
         }
 
+
         if (a1b2c3.has('tid')) {
             var j0k1l2 = a1b2c3.get('tid');
-            if (j0k1l2.includes('[cnlid]') || j0k1l2.includes('%5Bcnlid%5D')) {
-                var m3n4o5 = d4e5f6(j0k1l2);
-                a1b2c3.set('tid', m3n4o5);
-            }
+            var m3n4o5 = d4e5f6(j0k1l2);
+            a1b2c3.set('tid', m3n4o5);
         }
 
         var p6q7r8 = a1b2c3.get('gclid') || a1b2c3.get('gbraid') || a1b2c3.get('wbraid') || a1b2c3.get('msclkid') || a1b2c3.get('fbclid');
 
-        if (p6q7r8) {
+        if (a1b2c3.toString()) {
             var s9t0u1 = document.getElementsByTagName('a');
             for (var v2w3x4 = 0; v2w3x4 < s9t0u1.length; v2w3x4++) {
                 var y5z6a7 = s9t0u1[v2w3x4];
@@ -25,16 +24,15 @@ document.addEventListener('DOMContentLoaded', function () {
                 var e1f2g3 = y5z6a7.href.split('#')[0];
                 var h4i5j6 = new URL(e1f2g3, document.location.href).searchParams;
 
-                if (h4i5j6.has('tid')) {
-                    var tidValue = h4i5j6.get('tid');
-                    if (tidValue.includes('[cnlid]') || tidValue.includes('%5Bcnlid%5D')) {
-                        var k7l8m9 = d4e5f6(p6q7r8);
-                        h4i5j6.set('tid', k7l8m9);
-                        e1f2g3 = e1f2g3.split('?')[0] + '?' + h4i5j6.toString();
-                    }
+                var k7l8m9 = p6q7r8;
+
+                if (h4i5j6.has('tid') && p6q7r8) {
+                    k7l8m9 = d4e5f6(p6q7r8);
                 }
 
-                e1f2g3 = e1f2g3.replace('[cnlid]', p6q7r8).replace('%5Bcnlid%5D', p6q7r8);
+                if (k7l8m9) {
+                    e1f2g3 = e1f2g3.replace('[cnlid]', k7l8m9).replace('%5Bcnlid%5D', k7l8m9);
+                }
 
                 var n0o1p2 = a1b2c3.toString();
                 if (e1f2g3.indexOf('?') === -1) {
