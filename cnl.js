c4bb863272982121bb91778694dc5f43120d757c
opadim <152433032+opadim@users.noreply.github.com>
2024-09-03 20:13:31 +0100

    Handle tid included

--- cnl.js
+++ cnl.js
@@ -1,47 +1,45 @@
-// cannoli = awesomex10
+// cannoli = awesomex20240903
 document.addEventListener('DOMContentLoaded', function () {
     (function () {
-        var a1b2c3 = new URLSearchParams(window.location.search);
-
-        function d4e5f6(g7h8i9) {
-            return g7h8i9.replace(/ /g, '_s_').replace(/-/g, '_d_').replace(/\//g, '');
+        var urlParams = new URLSearchParams(window.location.search);
+        function encodeSpecialChars(str) {
+            return str.replace(/ /g, '_s_').replace(/-/g, '_d_').replace(/\//g, '');
         }
-
-
-        if (a1b2c3.has('tid')) {
-            var j0k1l2 = a1b2c3.get('tid');
-            var m3n4o5 = d4e5f6(j0k1l2);
-            a1b2c3.set('tid', m3n4o5);
-        }
-
-        var p6q7r8 = a1b2c3.get('gclid') || a1b2c3.get('gbraid') || a1b2c3.get('wbraid') || a1b2c3.get('msclkid') || a1b2c3.get('fbclid');
-
-        if (a1b2c3.toString()) {
-            var s9t0u1 = document.getElementsByTagName('a');
-            for (var v2w3x4 = 0; v2w3x4 < s9t0u1.length; v2w3x4++) {
-                var y5z6a7 = s9t0u1[v2w3x4];
-                var b8c9d0 = y5z6a7.hash;
-                var e1f2g3 = y5z6a7.href.split('#')[0];
-                var h4i5j6 = new URL(e1f2g3, document.location.href).searchParams;
-
-                var k7l8m9 = p6q7r8;
-
-                if (h4i5j6.has('tid') && p6q7r8) {
-                    k7l8m9 = d4e5f6(p6q7r8);
+        var trackingId = urlParams.get('gclid') || urlParams.get('gbraid') || urlParams.get('wbraid') || urlParams.get('msclkid') || urlParams.get('fbclid') || 'desconhecido';
+        
+        var links = document.getElementsByTagName('a');
+        for (var i = 0; i < links.length; i++) {
+            var link = links[i];
+            var hash = link.hash;
+            var href = link.href.split('#')[0];
+            var url = new URL(href, document.location.href);
+            var linkParams = url.searchParams;
+            
+            linkParams.forEach(function(value, key) {
+                if (value.includes('[cnlid]') || value.includes('%5Bcnlid%5D')) {
+                    var newValue = value.replace(/\[cnlid\]/g, trackingId).replace(/%5Bcnlid%5D/g, trackingId);
+                    if (key.toLowerCase() === 'tid') {
+                        newValue = encodeSpecialChars(newValue);
+                    }
+                    linkParams.set(key, newValue);
+                } else if (key.toLowerCase() === 'tid') {
+                    linkParams.set(key, encodeSpecialChars(value));
                 }
-
-                if (k7l8m9) {
-                    e1f2g3 = e1f2g3.replace('[cnlid]', k7l8m9).replace('%5Bcnlid%5D', k7l8m9);
-                }
-
-                var n0o1p2 = a1b2c3.toString();
-                if (e1f2g3.indexOf('?') === -1) {
-                    e1f2g3 += '?' + n0o1p2;
-                } else {
-                    e1f2g3 += '&' + n0o1p2;
-                }
-                y5z6a7.href = e1f2g3 + b8c9d0;
+            });
+            
+            if (urlParams.toString()) {
+                urlParams.forEach(function(value, key) {
+                    if (!linkParams.has(key)) {
+                        if (key.toLowerCase() === 'tid') {
+                            linkParams.set(key, encodeSpecialChars(value));
+                        } else {
+                            linkParams.set(key, value);
+                        }
+                    }
+                });
             }
+            
+            link.href = url.toString() + hash;
         }
     })();
 });
